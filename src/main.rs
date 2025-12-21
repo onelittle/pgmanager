ae0d87eb2eaae224ce5610671ce329e3e3c159f2
Theodor Tonum <theodor@tonum.no>
2025-12-21 11:30:38 +0100

    feat: add serve/wrap subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::{
     collections::VecDeque,
+    path::PathBuf,
     sync::{Arc, atomic::AtomicUsize},
 };
 
+use clap::{Parser, Subcommand, command};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt as _},
     net::UnixListener,
+    select,
     sync::Mutex,
 };
+use tracing::{debug, info};
 
 static USAGE: AtomicUsize = AtomicUsize::new(0);
 static PEAK_USAGE: AtomicUsize = AtomicUsize::new(0);
@@ -17,8 +21,7 @@ fn increment_usage() {
     let current = USAGE.fetch_add(1, std::sync::atomic::Ordering::Relaxed) + 1;
     let peak = PEAK_USAGE.load(std::sync::atomic::Ordering::Relaxed);
     if current > peak {
-        #[cfg(debug_assertions)]
-        eprintln!("Peak usage: {}", current);
+        debug!("Peak usage: {}", current);
         PEAK_USAGE.store(current, std::sync::atomic::Ordering::Relaxed);
     }
 }
@@ -27,16 +30,11 @@ fn decrement_usage() -> usize {
     USAGE.fetch_sub(1, std::sync::atomic::Ordering::Relaxed)
 }
 
-#[tokio::main]
-async fn main() {
-    // Open a Unix domain socket listener on {cwd}/tmp/test_manager.sock
-    let path = std::env::current_dir()
-        .unwrap()
-        .join("tmp")
-        .join("test_manager.sock");
-
-    let listener = UnixListener::bind(&path).unwrap();
-
+fn serve(
+    path: PathBuf,
+    cancellation_token: tokio_util::sync::CancellationToken,
+    barrier: Option<Arc<tokio::sync::Barrier>>,
+) -> tokio::task::JoinHandle<()> {
     let mut databases: VecDeque<String> = VecDeque::new();
     let max_count = std::env::var("DATABASE_COUNT")
         .map(|n| n.parse::<usize>().unwrap())
@@ -48,74 +46,153 @@ async fn main() {
 
     let databases = Arc::new(Mutex::new(databases));
 
+    if path.is_dir() {
+        panic!("Socket path cannot be a directory");
+    }
+    let parent_dir = path.parent().expect("Socket needs to be in a directory");
+    if !parent_dir.exists() {
+        std::fs::create_dir_all(parent_dir).unwrap();
+    }
+    let listener = UnixListener::bind(&path).unwrap();
     tokio::spawn(async move {
-        eprintln!("Listening on {:?}", path);
+        debug!("Listening on {}", path.display());
+        if let Some(barrier) = barrier {
+            barrier.wait().await;
+        }
         loop {
-            match listener.accept().await {
-                Ok((mut stream, addr)) => {
-                    let databases = databases.clone();
-                    tokio::spawn(async move {
-                        #[cfg(debug_assertions)]
-                        eprintln!("New connection from {:?}", addr);
-                        #[cfg(debug_assertions)]
-                        eprintln!("Assigning database...");
-                        let name = {
-                            loop {
-                                let mut dbs = databases.lock().await;
-                                if let Some(name) = dbs.pop_front() {
-                                    increment_usage();
-                                    break name.clone();
+            select! {
+                _ = cancellation_token.cancelled() => {
+                    break;
+                },
+                accept_result = listener.accept() => {
+                    match accept_result {
+                        Ok((mut stream, addr)) => {
+                            let databases = databases.clone();
+                            tokio::spawn(async move {
+                                debug!("New connection from {:?}", addr);
+                                debug!("Assigning database...");
+                                let name = {
+                                    loop {
+                                        let mut dbs = databases.lock().await;
+                                        if let Some(name) = dbs.pop_front() {
+                                            increment_usage();
+                                            break name.clone();
+                                        }
+                                        drop(dbs);
+                                        tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+                                        TOTAL_WAIT.fetch_add(10, std::sync::atomic::Ordering::Relaxed);
+                                    }
+                                };
+                                let instant = std::time::Instant::now();
+                                // Respont to the client OK:{db_name} or EMPTY:No databases available
+                                debug!("Assigned database: {:?}", name);
+                                if let Err(e) = stream.write_all(format!("OK:{}", name).as_bytes()).await {
+                                    debug!("Failed to write to stream: {}", e);
                                 }
-                                drop(dbs);
-                                tokio::time::sleep(std::time::Duration::from_millis(10)).await;
-                                TOTAL_WAIT.fetch_add(10, std::sync::atomic::Ordering::Relaxed);
-                            }
-                        };
-                        let instant = std::time::Instant::now();
-                        // Respont to the client OK:{db_name} or EMPTY:No databases available
-                        #[cfg(debug_assertions)]
-                        eprintln!("Assigned database: {:?}", name);
-                        if let Err(e) = stream.write_all(format!("OK:{}", name).as_bytes()).await {
-                            #[cfg(debug_assertions)]
-                            eprintln!("Failed to write to stream: {}", e);
-                        }
-                        stream.flush().await.unwrap();
+                                stream.flush().await.unwrap();
 
-                        let mut buffer = [0; 1024];
-                        if let Ok(0) = stream.read(&mut buffer).await {
-                            #[cfg(debug_assertions)]
-                            eprintln!("Client disconnected");
-                            #[cfg(debug_assertions)]
-                            eprintln!(
-                                "Releasing database: {} after {}ms usage",
-                                name,
-                                instant.elapsed().as_millis()
-                            );
-                            let mut dbs = databases.lock().await;
-                            dbs.push_back(name);
-                            decrement_usage();
+                                let mut buffer = [0; 1024];
+                                if let Ok(0) = stream.read(&mut buffer).await {
+                                    debug!("Client disconnected");
+                                    debug!(
+                                        "Releasing database: {} after {}ms usage",
+                                        name,
+                                        instant.elapsed().as_millis()
+                                    );
+                                    let mut dbs = databases.lock().await;
+                                    dbs.push_back(name);
+                                    decrement_usage();
+                                }
+                            });
                         }
-                    });
+                        Err(_) => { /* connection failed */ }
+                    }
                 }
-                Err(_) => { /* connection failed */ }
             }
         }
-    });
 
-    match tokio::signal::ctrl_c().await {
-        Ok(()) => {
-            eprintln!("Received shutdown signal, shutting down...");
-            eprintln!(
-                "Peak usage: {}",
-                PEAK_USAGE.load(std::sync::atomic::Ordering::Relaxed)
-            );
-            eprintln!(
-                "Total wait time: {}ms",
-                TOTAL_WAIT.load(std::sync::atomic::Ordering::Relaxed)
-            );
+        info!("Shutting down server...");
+        std::fs::remove_file(&path).unwrap();
+    })
+}
+
+#[derive(Parser)]
+struct Cli {
+    /// Path to the Unix domain socket
+    #[clap(short, long, default_value = "tmp/test_manager.sock")]
+    socket: String,
+    // Subcommand to wrap another command
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command()]
+    Serve,
+    #[command()]
+    Wrap {
+        #[arg(last = true)]
+        command: Vec<String>,
+    },
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let args = Cli::parse();
+
+    let path = if args.socket.starts_with("/") {
+        PathBuf::from(args.socket)
+    } else {
+        std::env::current_dir().unwrap().join(args.socket)
+    };
+
+    match args.command {
+        Commands::Serve => {
+            let cancellation_token = tokio_util::sync::CancellationToken::new();
+            let server = tokio::spawn(serve(path, cancellation_token.clone(), None));
+            match tokio::signal::ctrl_c().await {
+                Ok(()) => {
+                    info!("Received shutdown signal, shutting down...");
+                    cancellation_token.cancel();
+                    server.await.unwrap().unwrap();
+                    info!(
+                        "Peak usage: {}",
+                        PEAK_USAGE.load(std::sync::atomic::Ordering::Relaxed)
+                    );
+                    info!(
+                        "Total wait time: {}ms",
+                        TOTAL_WAIT.load(std::sync::atomic::Ordering::Relaxed)
+                    );
+                }
+                Err(err) => {
+                    info!("Unable to listen for shutdown signal: {}", err);
+                }
+            }
         }
-        Err(err) => {
-            eprintln!("Unable to listen for shutdown signal: {}", err);
+        Commands::Wrap { command } => {
+            let barrier = Arc::new(tokio::sync::Barrier::new(2));
+            let cancellation_token = tokio_util::sync::CancellationToken::new();
+            let server = tokio::spawn(serve(
+                path.clone(),
+                cancellation_token.clone(),
+                Some(barrier.clone()),
+            ));
+            barrier.wait().await;
+
+            // Run the command as passed and send PGMANAGER_SOCKET env var
+            let command = command.join(" ");
+            let mut cmd = tokio::process::Command::new("sh");
+            cmd.arg("-c").arg(&command);
+            cmd.env("PGMANAGER_SOCKET", path.to_str().unwrap());
+            let status = cmd.status().await.unwrap();
+            cancellation_token.cancel();
+            server.await.unwrap().unwrap();
+            std::process::exit(status.code().unwrap_or(1));
         }
     }
 }
