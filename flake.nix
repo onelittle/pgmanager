3ed67cb4f7ac57241d00012f4a02eaf236c1326d
Theodor Tonum <theodor@tonum.no>
2026-01-01 16:43:27 +0100

    chore: prepare version 0.2.0

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 
         packages.pgmanager = pkgs.rustPlatform.buildRustPackage {
           pname = "pgmanager";
-          version = "0.1.0";
+          version = "0.2.0";
 
           src = ./.;
 
