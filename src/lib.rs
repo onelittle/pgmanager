edca73dbb04f7fb6f7c40361dabb46f889f0f491
Theodor Tonum <theodor@tonum.no>
2026-01-01 16:22:05 +0100

    ref: extract commands to library

---
+++ src/lib.rs
@@ -0,0 +1,190 @@
+use std::{
+    collections::VecDeque,
+    path::{Path, PathBuf},
+    str::FromStr,
+    sync::{Arc, atomic::AtomicUsize},
+};
+
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt as _},
+    net::UnixListener,
+    select,
+    sync::Mutex,
+};
+use tokio_util::sync::CancellationToken;
+use tracing::{debug, info, warn};
+
+static USAGE: AtomicUsize = AtomicUsize::new(0);
+static PEAK_USAGE: AtomicUsize = AtomicUsize::new(0);
+static TOTAL_WAIT: AtomicUsize = AtomicUsize::new(0);
+
+fn increment_usage() {
+    let current = USAGE.fetch_add(1, std::sync::atomic::Ordering::Relaxed) + 1;
+    let peak = PEAK_USAGE.load(std::sync::atomic::Ordering::Relaxed);
+    if current > peak {
+        debug!("Peak usage: {}", current);
+        PEAK_USAGE.store(current, std::sync::atomic::Ordering::Relaxed);
+    }
+}
+
+fn decrement_usage() -> usize {
+    USAGE.fetch_sub(1, std::sync::atomic::Ordering::Relaxed)
+}
+
+fn get_prefixed_env_var(key: &str) -> Option<String> {
+    std::env::var(format!("PGM_{}", key))
+        .map_err(|e| match e {
+            std::env::VarError::NotPresent => {
+                warn!("Environment variable PMG_{} not found.", key);
+                warn!("  Falling back to {}.", key);
+            }
+            _ => {
+                warn!("{}", e);
+            }
+        })
+        .or_else(|_| std::env::var(key))
+        .map_err(|e| match e {
+            std::env::VarError::NotPresent => {
+                warn!("Environment variable {} not found", key);
+            }
+            _ => {
+                warn!("{}", e);
+            }
+        })
+        .ok()
+}
+
+fn env_var<T: FromStr>(key: &str) -> Option<T> {
+    get_prefixed_env_var(key).and_then(|v| v.parse().ok())
+}
+
+async fn server(
+    path: PathBuf,
+    databases: Arc<Mutex<VecDeque<String>>>,
+    cancellation_token: CancellationToken,
+    barrier: Arc<tokio::sync::Barrier>,
+) {
+    let listener = UnixListener::bind(path.clone()).unwrap();
+    debug!("Listening on {}", path.display());
+    barrier.wait().await;
+    loop {
+        select! {
+            _ = cancellation_token.cancelled() => {
+                break;
+            },
+            accept_result = listener.accept() => {
+                match accept_result {
+                    Ok((mut stream, addr)) => {
+                        let databases = databases.clone();
+                        tokio::spawn(async move {
+                            debug!("New connection from {:?}", addr);
+                            debug!("Assigning database...");
+                            let name = {
+                                loop {
+                                    let mut dbs = databases.lock().await;
+                                    if let Some(name) = dbs.pop_front() {
+                                        increment_usage();
+                                        break name.clone();
+                                    }
+                                    drop(dbs);
+                                    tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+                                    TOTAL_WAIT.fetch_add(10, std::sync::atomic::Ordering::Relaxed);
+                                }
+                            };
+                            let instant = std::time::Instant::now();
+                            // Respont to the client OK:{db_name} or EMPTY:No databases available
+                            debug!("Assigned database: {:?}", name);
+                            if let Err(e) = stream.write_all(format!("OK:{}", name).as_bytes()).await {
+                                debug!("Failed to write to stream: {}", e);
+                            }
+                            stream.flush().await.unwrap();
+
+                            let mut buffer = [0; 1024];
+                            if let Ok(0) = stream.read(&mut buffer).await {
+                                debug!("Client disconnected");
+                                debug!(
+                                    "Releasing database: {} after {}ms usage",
+                                    name,
+                                    instant.elapsed().as_millis()
+                                );
+                                let mut dbs = databases.lock().await;
+                                dbs.push_back(name);
+                                decrement_usage();
+                            }
+                        });
+                    }
+                    Err(_) => { /* connection failed */ }
+                }
+            }
+        }
+    }
+
+    info!("Shutting down server...");
+    std::fs::remove_file(&path).unwrap();
+}
+
+async fn start_server(path: &Path) -> (tokio::task::JoinHandle<()>, CancellationToken) {
+    let cancellation_token = tokio_util::sync::CancellationToken::new();
+    let barrier = Arc::new(tokio::sync::Barrier::new(2));
+    let max_count: usize = env_var("DATABASE_COUNT").unwrap_or(8);
+    let db_prefix: String = env_var("DATABASE_PREFIX").expect("DATABASE_PREFIX must be set");
+    let mut databases: VecDeque<String> = VecDeque::new();
+    for n in 0..max_count {
+        databases.push_back(format!("{}{}", db_prefix, n));
+    }
+
+    let databases = Arc::new(Mutex::new(databases));
+
+    if path.is_dir() {
+        panic!("Socket path cannot be a directory");
+    }
+    let parent_dir = path.parent().expect("Socket needs to be in a directory");
+    if !parent_dir.exists() {
+        std::fs::create_dir_all(parent_dir).unwrap();
+    }
+    let server = {
+        let path = path.to_path_buf();
+        let cancellation_token = cancellation_token.clone();
+        let barrier = barrier.clone();
+        tokio::spawn(server(path, databases, cancellation_token, barrier))
+    };
+    barrier.wait().await;
+    (server, cancellation_token)
+}
+
+pub async fn serve(path: &Path) {
+    let (server, cancellation_token) = start_server(path).await;
+
+    match tokio::signal::ctrl_c().await {
+        Ok(()) => {
+            info!("Received shutdown signal, shutting down...");
+            cancellation_token.cancel();
+            server.await.unwrap();
+            info!(
+                "Peak usage: {}",
+                PEAK_USAGE.load(std::sync::atomic::Ordering::Relaxed)
+            );
+            info!(
+                "Total wait time: {}ms",
+                TOTAL_WAIT.load(std::sync::atomic::Ordering::Relaxed)
+            );
+        }
+        Err(err) => {
+            info!("Unable to listen for shutdown signal: {}", err);
+        }
+    }
+}
+
+pub async fn wrap(path: &Path, command: Vec<String>) {
+    let (server, cancellation_token) = start_server(path).await;
+
+    // Run the command as passed and send PGMANAGER_SOCKET env var
+    let command = command.join(" ");
+    let mut cmd = tokio::process::Command::new("sh");
+    cmd.arg("-c").arg(&command);
+    cmd.env("PGMANAGER_SOCKET", path.to_str().unwrap());
+    let status = cmd.status().await.unwrap();
+    cancellation_token.cancel();
+    server.await.unwrap();
+    std::process::exit(status.code().unwrap_or(1));
+}
